2e95a255efc86e4fc8d68b076a1a2f94a7a5273c
Brandon <bbates377@gmail.com>
2020-10-19 18:52:29 -0500

    s

--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 const cassandra = require('cassandra-driver');
-const client = new cassandra.Client({contactPoints: ['162.203.169.116']});
+const client = new cassandra.Client({contactPoints: ['162.203.169.116'], localDataCenter: 'database1'});
 
 const testQuery = (id) => {
-  client.execute(`ALLOW FILTERING git pSELECT * FROM testqnas.questions WHERE id = ?;`, [id])
+  client.execute(`ALLOW FILTERING SELECT * FROM testqnas.questions WHERE id = ?;`, [id])
     .then(res => console.log(res))
 }
 
