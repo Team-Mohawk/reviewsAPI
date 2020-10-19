f2d79837c08bbae53c2637fea053965faa205a9b
Brandon <bbates377@gmail.com>
2020-10-19 18:53:20 -0500

    fix Data Center

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const cassandra = require('cassandra-driver');
-const client = new cassandra.Client({contactPoints: ['162.203.169.116'], localDataCenter: 'database1'});
+const client = new cassandra.Client({contactPoints: ['162.203.169.116'], localDataCenter: 'datacenter1'});
 
 const testQuery = (id) => {
   client.execute(`ALLOW FILTERING SELECT * FROM testqnas.questions WHERE id = ?;`, [id])
