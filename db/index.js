0a8b3d02a68b740b2340e78773d56f3ecc759981
Brandon <bbates377@gmail.com>
2020-10-19 17:26:30 -0500

    a

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const cassandra = require('cassandra-driver');
-const client = new cassandra.Client({contactPoints: ['localhost']});
+const client = new cassandra.Client({contactPoints: ['http://162.203.169.116/']});
 
 const testQuery = (id) => {
   client.execute(`SELECT * FROM questions WHERE id = ?`, [id])
