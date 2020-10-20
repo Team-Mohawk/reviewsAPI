5fce58b5223317a6062b297cd0d676fba9a9d40b
Brandon <bbates377@gmail.com>
2020-10-20 10:11:39 -0500

    test query

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({contactPoints: ['162.203.169.116'], localDataCenter: 'datacenter1'});
 
 const testQuery = (id) => {
-  client.execute(`ALLOW FILTERING SELECT * FROM testqnas.questions WHERE id = ?;`, [id])
+  client.execute(`SELECT * FROM testqnas.answers_photos WHERE answer_id = 5 AND id > 0;`)
     .then(res => return res)
 }
 
