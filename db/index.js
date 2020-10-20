84cc1bed76b30c09e16b043aa10598948a898d9c
Brandon <bbates377@gmail.com>
2020-10-20 10:12:49 -0500

    fix

--- db/index.js
+++ db/index.js
@@ -3,7 +3,7 @@ const client = new cassandra.Client({contactPoints: ['162.203.169.116'], localDa
 
 const testQuery = (id) => {
   client.execute(`SELECT * FROM testqnas.answers_photos WHERE answer_id = 5 AND id > 0;`)
-    .then(res => return res)
+    .then(res => { return res; })
 }
 
 module.exports = {
