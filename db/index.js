72e30bf4fa5e4da88b2fdb819516c08883f147ba
Brandon <bbates377@gmail.com>
2020-10-20 10:16:58 -0500

    fix

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({contactPoints: ['162.203.169.116'], localDataCenter: 'datacenter1'});
 
 const testQuery = () => {
-  client.execute(`SELECT * FROM testqnas.answers WHERE answer_id = 5 AND id > 0;`)
+  client.execute(`SELECT * FROM testqnas.answers WHERE question_id = 5 AND id > 0;`)
     .then(res => { return res; })
 }
 
