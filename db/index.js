2b8fc3caaa9ac200af3a9ac84bb3e65acc0f413c
Brandon <bbates377@gmail.com>
2020-10-20 10:24:28 -0500

    implement keyspace

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const cassandra = require('cassandra-driver');
-const client = new cassandra.Client({contactPoints: ['162.203.169.116'], localDataCenter: 'datacenter1'});
+const client = new cassandra.Client({contactPoints: ['162.203.169.116'], localDataCenter: 'datacenter1', keyspace: 'testqnas'});
 
 const testQuery = () => {
   client.execute(`SELECT * FROM testqnas.answers WHERE question_id = 5 AND id > 0`)
