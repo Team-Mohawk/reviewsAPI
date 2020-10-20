e2eda349aee4843affbe4c43f540ee42666a150d
Brandon <bbates377@gmail.com>
2020-10-20 10:28:33 -0500

    add catch block

--- db/index.js
+++ db/index.js
@@ -2,11 +2,12 @@ const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({contactPoints: ['162.203.169.116'], localDataCenter: 'datacenter1', keyspace: 'testqnas'});
 
 const testQuery = () => {
-  client.execute(`SELECT * FROM testqnas.answers WHERE question_id = 5 AND id > 0`)
+  client.execute('SELECT * FROM testqnas.answers WHERE question_id = 5 AND id > 0')
     .then(res => {
       console.log(res);
       return res;
     })
+    .catch(err => console.log(err));
 }
 
 module.exports = {
